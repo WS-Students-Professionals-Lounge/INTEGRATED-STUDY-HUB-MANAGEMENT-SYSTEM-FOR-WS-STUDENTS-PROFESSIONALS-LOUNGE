b69b0ac18c9390ae018de6ab7dc46eef7eeacad5
justinereyes25 <juga.reyes.ui@phinmaed.com>
2026-09-14 03:41:50 +0800

    Temporary resend email

--- high_end_ws_lounge/client_fixed.py
+++ high_end_ws_lounge/client_fixed.py
@@ -120,7 +120,7 @@ def forgot_password():
                 resend.api_key = os.environ.get("RESEND_API_KEY")
 
                 resend.Emails.send({
-                    "from": "WS Lounge <onboarding@resend.dev>",
+                    "from": "WS Lounge <noreply@wslounge.com>",
                     "to": [user.email],
                     "subject": "WS Lounge - Password Reset Request",
                     "html": f"""
